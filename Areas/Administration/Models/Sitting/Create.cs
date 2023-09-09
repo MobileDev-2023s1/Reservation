3062297cb6056928698b1c1984bd84baa05012c6
ttaakkaa <taka.tmail@gmail.com>
2023-09-09 17:07:56 +1000

    admin added

---
+++ Areas/Administration/Models/Sitting/Create.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Group_BeanBooking.Data;
+
+namespace Group_BeanBooking.Areas.Administration.Models.Sitting
+{
+    public class Create
+    {
+      
+        public string Name { get; set; }
+
+
+        public bool Closed { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int TypeId { get; set; }
+        public SelectList? SittingTypes { get; set; }
+       
+    }
+}
---
+++ Areas/Administration/Models/Sitting/Edit.cs
@@ -0,0 +1,7 @@
+﻿namespace Group_BeanBooking.Areas.Administration.Models.Sitting
+{
+    public class Edit:Create
+    {
+        public int Id { get; set; }
+    }
+}
